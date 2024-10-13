a187c8286aa5f4cd2d4e24d0f09dd600de34cf73
Nick <nicholaspetaccio@gmail.com>
2024-10-13 04:51:34 -0400

    Replaced multiple if statements with switch case statement to improve readability, also added error handling for when a user tries to divide by 0.

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ let currentNumber = [];
 let firstNumber;
 let secondNumber;
 let operation;
-let equation;
-//Handle displaying clicked numbers on calculator display
+let result;
+//Handle displaying clicked numbers on calculator display.
 const handleClick = val => {
+    //handle for case in which a user tries to input multiple decimal points.
+    if(val === '.' && currentNumber.includes('.')){
+        return;
+    }
+    //handle for when the first number has a value and requires a value for the second number.
     if(!firstNumber){
     currentNumber.push(val);
     document.getElementById("display").value = currentNumber.join('');
     }else{
         currentNumber.push(val);
         document.getElementById("display").value = currentNumber.join('');
+    }
+    //handle chaining previous results with further arithmetic.
+    if(result){
+        firstNumber = result;
+        secondNumber = document.getElementById('display').value;
     }
 }
 //handle choosing the operation and inputting the second number to apply arithmetic to.
@@ -22,15 +32,28 @@ const handleOp = op => {
 //handle displaying the results of the equation on the calculator.
 const handleEquals = () => {
     secondNumber = currentNumber.join('');
-    if(operation === '+'){
-        document.getElementById('display').value = Number(firstNumber) + Number(secondNumber);
-    }else if(operation === '-'){
-        document.getElementById('display').value = Number(firstNumber) - Number(secondNumber);
-    }else if(operation === '*'){
-        document.getElementById('display').value = Number(firstNumber) * Number(secondNumber);
+    //handle edge case if user tries to divide by 0
+    if(operation === '/' && Number(firstNumber) === 0 || operation === '/' && Number(secondNumber) === 0){
+        return document.getElementById('display').value = 'Err - Cannot divide by 0';
     }else{
-        document.getElementById('display').value = Number(firstNumber) / Number(secondNumber);
+    switch(operation) {
+        case '+':
+            document.getElementById('display').value = Number(firstNumber) + Number(secondNumber);
+            break;
+        case '-':
+            document.getElementById('display').value = Number(firstNumber) - Number(secondNumber);
+            break;
+        case '*':
+            document.getElementById('display').value = Number(firstNumber) * Number(secondNumber);
+            break;
+        case '/':
+            document.getElementById('display').value = Number(firstNumber) / Number(secondNumber);
+            break;
+        default:
+            document.getElementById('display').value = 'Err';
+        }
     }
+    result = document.getElementById('display').value;
 }
 //handle resetting the values in the calculator
 const handleClear = () => {
